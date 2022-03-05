3500b7f71523efe2503a30d852cdc1af717bef7d
836334258 <836334258@qq.com>
2022-03-05 13:41:54 +0800

    ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ import pkg from './package.json'
 import path from 'path'
 
 export default {
-  mode: 'production',
-  entry: path.resolve('src', 'index.js'),
+  mode: 'development',
+  entry: path.resolve('src', 'index.ts'),
   output: {
     filename: `${pkg.name}.min.js`,
     path: path.resolve('dist'),
     library: {
       name: `${pkg.name}`,
       type: 'umd',
+      export: 'default', // 不使用default会导致kingutils.hello()出错
     },
   },
   module: {
@@ -21,6 +22,19 @@ export default {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.mjs'], // 引入文件可以不用加后缀名
+    alias: {
+      '@': path.resolve('src'),
+    },
   },
 }
